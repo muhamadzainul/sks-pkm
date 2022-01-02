9fef800dc9943219963e1f054f700ed0279cabe8
muhamadzainul <zainulmustofa943@gmail.com>
2022-01-02 16:18:19 +0700

    sql

--- sks_pkm.sql
+++ sks_pkm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 02, 2022 at 04:22 AM
+-- Generation Time: Jan 02, 2022 at 10:17 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.26
 
@@ -193,7 +193,8 @@ INSERT INTO `auth_logins` (`id`, `ip_address`, `email`, `user_id`, `date`, `succ
 (74, '::1', 'zainulmustofa943@gmail.com', 4, '2022-01-01 07:01:19', 1),
 (75, '::1', 'zainulmuhamad84@gmail.com', 5, '2022-01-01 08:08:24', 1),
 (76, '::1', 'zainulmustofa943@gmail.com', 4, '2022-01-01 08:19:07', 1),
-(77, '::1', 'zainulmustofa943@gmail.com', 4, '2022-01-01 21:12:21', 1);
+(77, '::1', 'zainulmustofa943@gmail.com', 4, '2022-01-01 21:12:21', 1),
+(78, '::1', 'zainulmustofa943@gmail.com', 4, '2022-01-02 00:42:13', 1);
 
 -- --------------------------------------------------------
 
@@ -318,6 +319,7 @@ CREATE TABLE `pasien` (
   `nama_pasien` varchar(255) NOT NULL,
   `slug` varchar(255) NOT NULL,
   `nik_pasien` bigint(20) DEFAULT NULL,
+  `nip_pasien` bigint(50) DEFAULT NULL,
   `tgl_lahir` date DEFAULT NULL,
   `jenis_kelamin` varchar(255) NOT NULL,
   `alamat` varchar(255) DEFAULT NULL,
@@ -336,19 +338,19 @@ CREATE TABLE `pasien` (
 -- Dumping data for table `pasien`
 --
 
-INSERT INTO `pasien` (`id_pasien`, `nama_pasien`, `slug`, `nik_pasien`, `tgl_lahir`, `jenis_kelamin`, `alamat`, `no_hp`, `email`, `publik_key`, `private_key`, `qr_code`, `foto_ktp`, `foto_kk`, `tanggal_dibuat`, `tanggal_diubah`) VALUES
-(42, 'Siska', '1234123412341234-siska', 1234123412341234, '1997-05-13', 'Perempuan', 'gogor', 54672354567, 'jensyn.ryuu@fineoak.org', '293.65311', '5/lsXsmlYe/GFbuirg/9yTw/CjHTf174Fl2Ww6Jk44w5e4lp6U54XC0pt2D1353XFQIt9QplC4gd+bkh/8rePfOzRQozUemWIAgBimKLLIaujc2kI4L5LM91NA==', 'rM9Yc2Nn6Lzal2j7z5HsFc8D3NKhPyB9.png', 'default-ktp.png', 'default-kk.png', '2021-10-21', '2021-12-18'),
-(64, 'saman', '2344561234561234-saman', 2344561234561234, '1980-08-15', 'Laki-laki', 'gresik', NULL, NULL, '151.77057', 'GhfmcXPGj4pMxH1VPm/d4LPLtW3O+3NGvzjRuXCyXifWSd7tqyzTxYtZq7L0Pa8FoWELTpz5XRgJ+cLf0ugMBopgEqXkFuwCBL9WLmtNS6ioGKW1TX1lP6gdkA==', 'gnUB3dGDAdXBQSdazeDMB5vpx6prnmfV.png', 'default-ktp.png', 'default-kk.png', '2021-12-03', '2021-12-03'),
-(70, 'Muhamad Zainul', '3516170980980981-muhamad-zainul', 3516170980980981, '1999-08-09', 'Laki-laki', 'Dsn. Brayu wetan, 03/05, Brayublandong, Dawarblandong', 1235431235, 'asa@gkl.com', '251.133907', '7Me0ECguimoUwWyDDG4NJxj3Oy8tRADJ/G3JC6HdWXjxeIJqjbY3ReKS6VY8a7nvE1GHDxQWSjhCOK7pEwQthAyuLysZKmyZrGgU/dacac9AxKJljsLzJc/Y9tA=', 'iWlvtIUclqBxr7yx6KcEai1oh7qo965C.png', 'KTP - 1639922035_7b64e90dab229500b0ad.png', 'KK - 1639922035_7b64e90dab229500b0ad.png', '2021-12-19', '2021-12-31'),
-(72, 'korin', '872634756421334-korin', 872634756421334, '2002-09-10', 'Perempuan', 'Gersik', NULL, NULL, '373.82933', 'OCCzfj03MCHHVUnFXDVMg/QiyHDK80p4Q9bMm+EhaqyGrXegiP2SgXJTmhCpC55u0W5E4XYgzVtGnXv+OjOUFLprdXJjPQ21AwpphgyB9Bv614QyE6YDCg2l9g==', 'c1H5qKyFBMzHsiNCXIETr5ktxrg1xh0Q.png', 'default-ktp.png', 'default-kk.png', '2021-12-20', '2021-12-20'),
-(76, 'karisma', '7653289429835482-karisma', 7653289429835482, '2000-02-19', 'Perempuan', 'Gersik', NULL, NULL, '367.86747', 'ZpVb+zm6lpEsMO7LnWfnICbhv5odTNJT1O6aqUWLkHqobj9Q2byvtY9s+qiETs8a1CJQn3HKjyU2odVTwSnYbnqkelUsCMDZxM2TEkbGMVHIMb1yiyL/Hqic0A==', 'eS9I6J39leYAXB5rVousJLo6OU0DKzpQ.png', 'default-ktp.png', 'default-kk.png', '2021-12-21', '2021-12-21'),
-(77, 'muslimin', '3516170908990002-muslimin', 3516170908990002, '1999-08-09', 'Laki-laki', 'Lamongan', NULL, NULL, '277.99899', 'efqG4jM55qeu7K/uGT6Wc/afTCthhd95eXvMT2ny/XsW57pW1CWljtHbJURVfQDsJO2Fh1AohM3vHyZtCacVh0jLC3709cDMVmJfnbvUdpUQL//679vNUJDgyQ==', 'hCPhJguibBtlnVCC1m0BVtKlUwuarsL8.png', 'default-ktp.png', 'default-kk.png', '2021-12-26', '2021-12-26'),
-(79, 'Khairi Ramadhan', '678324562345322-khairi-ramadhan', 678324562345322, '1998-12-12', 'Laki-laki', 'Madura', 234134214124, 'khairi@gmail.com', '127.98767', 'V99O8QON7nRNChage7e+ueCViqyC7eeUAqRkgpJr0YjSqfTK3sRCBHIwN300KQlvKhtKiNKaE+JbJ0vo7u65sPfc9XWMlTZfjG2E27AEzXn+LOU4B423hQ8pZwY=', 'AHyxkV1i69YMI2rATICpfkhZG5H34725.png', 'default-ktp.png', 'default-kk.png', '2021-12-28', '2021-12-28'),
-(80, 'malikin', '7657623549876231-malikin', 7657623549876231, '1989-07-18', 'Laki-laki', 'Lamongan', NULL, NULL, '239.154433', 'gaRSFRPsadpvCo/f0BPhYOzAaEPm2zUUdb1e6GQpzqyb3M3ID+7eMVPhewhtjB9JC94JbagU4/qQuyOuo285YVTlWGO+afiHmw4dGKbA6VGHQVGfKhgmE/2jKng=', 'ic6Du37BtArshMIpr1cjiOSdYqMzQef4.png', 'default-ktp.png', 'default-kk.png', '2021-12-28', '2021-12-28'),
-(81, 'mustakim', '761283012536123-mustakim', 761283012536123, '1978-08-19', 'Laki-laki', 'mojokerto', NULL, NULL, '127.82919', 'TI9J3LEhv4fwaKzs9BfQveguWngHbBBOd4aQKeXn7cG2W2JNcncVpqC8mpAda3c3f/fDjVj6hgzSJVNsvCo+bEFrdpiWtfd5ZB8rmw8uTdiZFmdGF+e4zQh8CA==', '2NKWoywPqLelBynlxPR7GZ85iwdYMlRx.png', 'default-ktp.png', 'default-kk.png', '2021-12-28', '2021-12-28'),
-(82, 'salimin', '645367218613123-salimin', 645367218613123, '1998-09-17', 'Laki-laki', 'Madura', 98203748923, 'jasghd@gmail.com', '293.87953', '2ueCnh11qQexmrrvQXfBTee5WvBc7EfNZdkXUXF76ZQc/JeBIuEo4QVwkBrQk0G4VYYflmg+0GMivjB5k764ptqqHPNc/8JFHUX+R8o7avkTD4DoQq0RehYrwQ==', '9PNQRbckUg2SlVodAtYVYRHx7GaicJi7.png', 'default-ktp.png', 'default-kk.png', '2021-12-29', '2021-12-29'),
-(83, 'malumah', '761523418723123123-malumah', 761523418723123123, '1998-08-19', 'Perempuan', 'Madura', NULL, NULL, '137.41707', 'UNCjTGFEPeBwEDewdfElaUUgQ1o0fqk2LmK3AaML8QAS9gEQkP2Rn0hqtLRGrpHkjKCdWVneP2jXZgx1Z/+buqR/vVdCmwW+QzMomUQksdRQuXRCYiSucTMtmg==', 'ZT44q2uE4huk8Ebv3y5XrIprhe7Ot5wW.png', 'default-ktp.png', 'default-kk.png', '2021-12-29', '2021-12-29'),
-(84, 'fathullah', '312375731986235123-fathullah', 312375731986235123, '1998-07-19', 'Laki-laki', 'Dsn. Wotgaru, 03/05, Pucuk, Dawarblandong', NULL, NULL, '233.46513', 'VfqzClKBfMK2WlipwbN1hg5j3cPEtZm5Rk5HQqDL9C6Y5bKIFvStVtEBqrcmNLjJEuWxg/o8Kb7Tvgbc+JoBvLUHjCrqQU8yMagwRW9789QEsuZ7JroCY/E37Q==', '', 'default-ktp.png', 'default-kk.png', '2022-01-01', '2022-01-01');
+INSERT INTO `pasien` (`id_pasien`, `nama_pasien`, `slug`, `nik_pasien`, `nip_pasien`, `tgl_lahir`, `jenis_kelamin`, `alamat`, `no_hp`, `email`, `publik_key`, `private_key`, `qr_code`, `foto_ktp`, `foto_kk`, `tanggal_dibuat`, `tanggal_diubah`) VALUES
+(42, 'Siska', '1234123412341234-siska', 1234123412341234, 12736187637812563, '1997-05-13', 'Perempuan', 'gogor', 54672354567, 'jensyn.ryuu@fineoak.org', '293.65311', '5/lsXsmlYe/GFbuirg/9yTw/CjHTf174Fl2Ww6Jk44w5e4lp6U54XC0pt2D1353XFQIt9QplC4gd+bkh/8rePfOzRQozUemWIAgBimKLLIaujc2kI4L5LM91NA==', 'rM9Yc2Nn6Lzal2j7z5HsFc8D3NKhPyB9.png', 'default-ktp.png', 'default-kk.png', '2021-10-21', '2022-01-02'),
+(64, 'saman', '2344561234561234-saman', 2344561234561234, 12736187637812563, '1980-08-15', 'Laki-laki', 'gresik', NULL, NULL, '151.77057', 'GhfmcXPGj4pMxH1VPm/d4LPLtW3O+3NGvzjRuXCyXifWSd7tqyzTxYtZq7L0Pa8FoWELTpz5XRgJ+cLf0ugMBopgEqXkFuwCBL9WLmtNS6ioGKW1TX1lP6gdkA==', 'gnUB3dGDAdXBQSdazeDMB5vpx6prnmfV.png', 'default-ktp.png', 'default-kk.png', '2021-12-03', '2022-01-02'),
+(70, 'Muhamad Zainul', '3516170980980981-muhamad-zainul', 3516170980980981, 12736187637812563, '1999-08-09', 'Laki-laki', 'Dsn. Brayu wetan, 03/05, Brayublandong, Dawarblandong', 1235431235, 'asa@gkl.com', '251.133907', '7Me0ECguimoUwWyDDG4NJxj3Oy8tRADJ/G3JC6HdWXjxeIJqjbY3ReKS6VY8a7nvE1GHDxQWSjhCOK7pEwQthAyuLysZKmyZrGgU/dacac9AxKJljsLzJc/Y9tA=', 'iWlvtIUclqBxr7yx6KcEai1oh7qo965C.png', 'KTP - 1639922035_7b64e90dab229500b0ad.png', 'KK - 1639922035_7b64e90dab229500b0ad.png', '2021-12-19', '2022-01-02'),
+(72, 'korin', '872634756421334-korin', 872634756421334, 12736187637812563, '2002-09-10', 'Perempuan', 'Gersik', NULL, NULL, '373.82933', 'OCCzfj03MCHHVUnFXDVMg/QiyHDK80p4Q9bMm+EhaqyGrXegiP2SgXJTmhCpC55u0W5E4XYgzVtGnXv+OjOUFLprdXJjPQ21AwpphgyB9Bv614QyE6YDCg2l9g==', 'c1H5qKyFBMzHsiNCXIETr5ktxrg1xh0Q.png', 'default-ktp.png', 'default-kk.png', '2021-12-20', '2022-01-02'),
+(76, 'karisma', '7653289429835482-karisma', 7653289429835482, 12736187637812563, '2000-02-19', 'Perempuan', 'Gersik', NULL, NULL, '367.86747', 'ZpVb+zm6lpEsMO7LnWfnICbhv5odTNJT1O6aqUWLkHqobj9Q2byvtY9s+qiETs8a1CJQn3HKjyU2odVTwSnYbnqkelUsCMDZxM2TEkbGMVHIMb1yiyL/Hqic0A==', 'eS9I6J39leYAXB5rVousJLo6OU0DKzpQ.png', 'default-ktp.png', 'default-kk.png', '2021-12-21', '2022-01-02'),
+(77, 'muslimin', '3516170908990002-muslimin', 3516170908990002, 12736187637812563, '1999-08-09', 'Laki-laki', 'Lamongan', NULL, NULL, '277.99899', 'efqG4jM55qeu7K/uGT6Wc/afTCthhd95eXvMT2ny/XsW57pW1CWljtHbJURVfQDsJO2Fh1AohM3vHyZtCacVh0jLC3709cDMVmJfnbvUdpUQL//679vNUJDgyQ==', 'hCPhJguibBtlnVCC1m0BVtKlUwuarsL8.png', 'default-ktp.png', 'default-kk.png', '2021-12-26', '2022-01-02'),
+(79, 'Khairi Ramadhan', '678324562345322-khairi-ramadhan', 678324562345322, 12736187637812563, '1998-12-12', 'Laki-laki', 'Madura', 234134214124, 'khairi@gmail.com', '127.98767', 'V99O8QON7nRNChage7e+ueCViqyC7eeUAqRkgpJr0YjSqfTK3sRCBHIwN300KQlvKhtKiNKaE+JbJ0vo7u65sPfc9XWMlTZfjG2E27AEzXn+LOU4B423hQ8pZwY=', 'AHyxkV1i69YMI2rATICpfkhZG5H34725.png', 'default-ktp.png', 'default-kk.png', '2021-12-28', '2022-01-02'),
+(80, 'malikin', '7657623549876231-malikin', 7657623549876231, 12736187637812563, '1989-07-18', 'Laki-laki', 'Lamongan', NULL, NULL, '239.154433', 'gaRSFRPsadpvCo/f0BPhYOzAaEPm2zUUdb1e6GQpzqyb3M3ID+7eMVPhewhtjB9JC94JbagU4/qQuyOuo285YVTlWGO+afiHmw4dGKbA6VGHQVGfKhgmE/2jKng=', 'ic6Du37BtArshMIpr1cjiOSdYqMzQef4.png', 'default-ktp.png', 'default-kk.png', '2021-12-28', '2022-01-02'),
+(81, 'mustakim', '761283012536123-mustakim', 761283012536123, 12736187637812563, '1978-08-19', 'Laki-laki', 'mojokerto', NULL, NULL, '127.82919', 'TI9J3LEhv4fwaKzs9BfQveguWngHbBBOd4aQKeXn7cG2W2JNcncVpqC8mpAda3c3f/fDjVj6hgzSJVNsvCo+bEFrdpiWtfd5ZB8rmw8uTdiZFmdGF+e4zQh8CA==', '2NKWoywPqLelBynlxPR7GZ85iwdYMlRx.png', 'default-ktp.png', 'default-kk.png', '2021-12-28', '2022-01-02'),
+(82, 'salimin', '645367218613123-salimin', 645367218613123, 12736187637812563, '1998-09-17', 'Laki-laki', 'Madura', 98203748923, 'jasghd@gmail.com', '293.87953', '2ueCnh11qQexmrrvQXfBTee5WvBc7EfNZdkXUXF76ZQc/JeBIuEo4QVwkBrQk0G4VYYflmg+0GMivjB5k764ptqqHPNc/8JFHUX+R8o7avkTD4DoQq0RehYrwQ==', '9PNQRbckUg2SlVodAtYVYRHx7GaicJi7.png', 'default-ktp.png', 'default-kk.png', '2021-12-29', '2022-01-02'),
+(83, 'malumah', '761523418723123123-malumah', 761523418723123123, 12736187637812563, '1998-08-19', 'Perempuan', 'Madura', NULL, NULL, '137.41707', 'UNCjTGFEPeBwEDewdfElaUUgQ1o0fqk2LmK3AaML8QAS9gEQkP2Rn0hqtLRGrpHkjKCdWVneP2jXZgx1Z/+buqR/vVdCmwW+QzMomUQksdRQuXRCYiSucTMtmg==', 'ZT44q2uE4huk8Ebv3y5XrIprhe7Ot5wW.png', 'default-ktp.png', 'default-kk.png', '2021-12-29', '2022-01-02'),
+(84, 'fathullah', '312375731986235123-fathullah', 312375731986235123, 12736187637812563, '1998-07-19', 'Laki-laki', 'Dsn. Wotgaru, 03/05, Pucuk, Dawarblandong', NULL, NULL, '233.46513', 'VfqzClKBfMK2WlipwbN1hg5j3cPEtZm5Rk5HQqDL9C6Y5bKIFvStVtEBqrcmNLjJEuWxg/o8Kb7Tvgbc+JoBvLUHjCrqQU8yMagwRW9789QEsuZ7JroCY/E37Q==', '', 'default-ktp.png', 'default-kk.png', '2022-01-01', '2022-01-02');
 
 -- --------------------------------------------------------
 
@@ -379,6 +381,36 @@ INSERT INTO `satgas` (`id_satgas`, `nip_petugas`, `nik_petugas`, `nama_petugas`,
 (2, 120398120938120398, 1203981239081098, 'Tyan Sah', '1203981239081098-tyan-sah', 'Laki-laki', 'gresik', 9831234123, 'gakun7020@gmail.com', '1634396696_9ab4ea3a2ffbf61cd118.jpg', '2021-10-15', '2021-10-16'),
 (3, 12341234124114, 12341234123412124, 'susanti ari', '12341234123412124-susanti-ari', '-- Pilih Jenis Kelamin --', 'mojokerto', 12334523345, 'iluljrdev@gmail.com', '1639729515_cd1536cd6f7912630798.png', '2021-10-21', '2021-12-17'),
 (4, 2341243213412432314, 234123423412432134, 'susanti ari', '234123423412432134-susanti-ari', 'Laki-laki', 'mojokerto', 2342312343, 'zainulmuhamad84@gmail.com', NULL, '2021-12-01', '2021-12-01');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `surat_izin`
+--
+
+CREATE TABLE `surat_izin` (
+  `id_suratIzin` int(11) NOT NULL,
+  `nomor_surat` bigint(20) NOT NULL,
+  `nik_pasien` bigint(20) NOT NULL,
+  `nip_kapus` bigint(20) NOT NULL,
+  `pangkat` varchar(20) NOT NULL,
+  `jabatan` varchar(50) NOT NULL,
+  `hari` varchar(15) NOT NULL,
+  `tanggal` date NOT NULL,
+  `kepentingan` varchar(255) NOT NULL,
+  `qr_code` varchar(255) NOT NULL,
+  `tgl_dibuat` date NOT NULL,
+  `tgl_diubah` date NOT NULL,
+  `tgl_exp` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `surat_izin`
+--
+
+INSERT INTO `surat_izin` (`id_suratIzin`, `nomor_surat`, `nik_pasien`, `nip_kapus`, `pangkat`, `jabatan`, `hari`, `tanggal`, `kepentingan`, `qr_code`, `tgl_dibuat`, `tgl_diubah`, `tgl_exp`) VALUES
+(3, 8, 3516170980980981, 198203102009011011, 'ruang 1', 'Admin', 'Kamis', '2022-01-01', 'izin sakit', 'd7a8c3ec9c9f5c6afef497e110941259.png', '2022-01-02', '2022-01-02', '0000-00-00'),
+(4, 2, 761283012536123, 198203102009011011, 'ruang 1', 'Admin', 'Selasa', '2022-01-03', 'izin sakit', 'ba77e7ce5d33ca5a6d67cad4ce4c849f.png', '2022-01-02', '2022-01-02', '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -450,6 +482,33 @@ INSERT INTO `surat_rsa` (`id_surat_rsa`, `nomor_surat`, `nik_pasien`, `nip_kapus
 (26, '3242', 761523418723123123, 198203102009011011, '0857cee0a4eb64abfec9b8b564e26fdd', '34594*36966.27256*12178.7885*38715.37338*4692.7026*12178.6715*38715.37397*12178.17656.2107*12569.0_30458*38715.16680*1.28853.31534*4692.3725*4692.16233*4692.3833*4692.9987.00_27819*12178.0000_0', '4a6f104332b5dc6eb94316f50d352d19', '+KSEhqpp6CZbbf+208sGKZbiCINrVSGIxraqEQzcfya6OX8uqoMfmS1Kla/ntLRlCFe9kfbqPlmYJdluTVCBqZK9fJB49ybZWqrGFEao71p1cW7djT/1J7o=', '2021-12-30', '2021-12-30'),
 (28, '13', 3516170980980981, 198203102009011011, '5dd61fd49d3649528fdba48329090eda', '91008*1.0_4516*27501.57699*46273.59811.00_2498*120651.55650.0_56691*120651.100072*5778.41789*20846.0_55048*1.0_78682*27501.0_20552*1.41199*20846.88032*5778.0_124546*29979.106377*46273.32246*27501.11158*5778.123533*0', '0b747ca568aa33981f57762a94a9a31e', 'skp+ZcF3CyRLTmSqNr/BWkmSlC8mXigH18RJz3KXdzf0nj+C/KslKuM+sg8NSKVBZ0exJ4Ehr+P6u+ffGVJeMNQ7ro6YUg2ShHpuC+UDiTnQI0H8PQD7G7Q=', '2021-12-31', '2021-12-31'),
 (29, '15', 312375731986235123, 198203102009011011, '74b7677deb23f9978a627a22e1eb3f8f', '44214*22758.5527*22758.9021*25859.44001*25859.0_27369*34230.36445*25859.8650*25859.43398*22758.22196*6480.18704.14757*25859.10883*17079.0_43980*17079.0_45749*0.30898*17079.36021*0.0_45370*4926.23531*0', '05eedfd95a980e04d6759682085eedf4', 'xp0rfGD3EUdt16QSvSxDVnY64vwiZRte+SluqDd+TnyJ6rtQSaSfDq7QmHxHsEzrWZjOsJa0sImRkvUmTXY1bLgGlb6BGW4RcpDRZJUY/ER1eZF5b582dhc=', '2022-01-01', '2022-01-01');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `surat_rsaizin`
+--
+
+CREATE TABLE `surat_rsaizin` (
+  `id_rsaIzin` int(11) NOT NULL,
+  `nomor_surat` bigint(20) NOT NULL,
+  `nik_pasien` bigint(20) NOT NULL,
+  `nip_kapus` bigint(20) NOT NULL,
+  `hash_text` varchar(255) NOT NULL,
+  `enkripsi_text` varchar(500) NOT NULL,
+  `hash_enkrip` varchar(128) NOT NULL,
+  `kunci_pass` varchar(500) NOT NULL,
+  `tgl_dibuat` date NOT NULL,
+  `tgl_diubah` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `surat_rsaizin`
+--
+
+INSERT INTO `surat_rsaizin` (`id_rsaIzin`, `nomor_surat`, `nik_pasien`, `nip_kapus`, `hash_text`, `enkripsi_text`, `hash_enkrip`, `kunci_pass`, `tgl_dibuat`, `tgl_diubah`) VALUES
+(1, 8, 3516170980980981, 198203102009011011, 'd7a8c3ec9c9f5c6afef497e110941259', '47329*46273.101198*20846.45843*5778.20672*29979.125940.125940.22989*0.10457*46273.58846*46273.0_78682*27501.28525*20846.127171*33167.5019*35946.103156*5778.56752*5778.126820*33167.96236*120651.117064*1', '5d4f5f855494251fe654b794931d5e78', 'D939DZvv8B3u3//EA8DmJrHz6ZJOMNyAYWWeOFkUMMSJa0ZkcWWnhEQjB9Cw/4bb7sElBX8z27ZLIqOWboP9mjk4/4qrN/4QLmkTEXZqTsG4S5FgeKq6ly4=', '2022-01-02', '2022-01-02'),
+(2, 2, 761283012536123, 198203102009011011, 'ba77e7ce5d33ca5a6d67cad4ce4c849f', '58653*62590.58661*82853.23583*48123.81755.0_3763*82853.31542*48123.76768*48123.60561*48123.6384*62590.57475*34454.48204*69234.26352.31542*48123.41367*69234.0_44788*48123.41624*34454.38839*34454.77303*0', '12a5354e8dee515c5ba1ff57ecb28496', 'M4ixtLjoq/2vocuEKpxHIFyQ6utvDK7lMzZHOoXNUe3DKtFkj5STRqJeHJBoHbK/eksWPnHPI2nVthgsCNnhR1FyCl0XSJI3a8PMDLvmJp1k+aY+Bios9/M=', '2022-01-02', '2022-01-02');
 
 -- --------------------------------------------------------
 
@@ -591,6 +650,15 @@ ALTER TABLE `satgas`
   ADD PRIMARY KEY (`id_satgas`);
 
 --
+-- Indexes for table `surat_izin`
+--
+ALTER TABLE `surat_izin`
+  ADD PRIMARY KEY (`id_suratIzin`),
+  ADD UNIQUE KEY `nomor_surat` (`nomor_surat`),
+  ADD KEY `FK_nik` (`nik_pasien`),
+  ADD KEY `FK_nipKp` (`nip_kapus`);
+
+--
 -- Indexes for table `surat_kesehatan`
 --
 ALTER TABLE `surat_kesehatan`
@@ -608,6 +676,15 @@ ALTER TABLE `surat_rsa`
   ADD KEY `FK_rsa_surat` (`nomor_surat`),
   ADD KEY `FK_rsa_kapus` (`nip_kapus`),
   ADD KEY `FK_rsa_pasien` (`nik_pasien`);
+
+--
+-- Indexes for table `surat_rsaizin`
+--
+ALTER TABLE `surat_rsaizin`
+  ADD PRIMARY KEY (`id_rsaIzin`),
+  ADD KEY `FK_nomor_surat` (`nomor_surat`),
+  ADD KEY `FK_nik_pas` (`nik_pasien`),
+  ADD KEY `FK_nip_kap` (`nip_kapus`);
 
 --
 -- Indexes for table `users`
@@ -643,7 +720,7 @@ ALTER TABLE `auth_groups`
 -- AUTO_INCREMENT for table `auth_logins`
 --
 ALTER TABLE `auth_logins`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=79;
 
 --
 -- AUTO_INCREMENT for table `auth_permissions`
@@ -688,6 +765,12 @@ ALTER TABLE `satgas`
   MODIFY `id_satgas` bigint(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
+-- AUTO_INCREMENT for table `surat_izin`
+--
+ALTER TABLE `surat_izin`
+  MODIFY `id_suratIzin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `surat_kesehatan`
 --
 ALTER TABLE `surat_kesehatan`
@@ -698,6 +781,12 @@ ALTER TABLE `surat_kesehatan`
 --
 ALTER TABLE `surat_rsa`
   MODIFY `id_surat_rsa` bigint(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- AUTO_INCREMENT for table `surat_rsaizin`
+--
+ALTER TABLE `surat_rsaizin`
+  MODIFY `id_rsaIzin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -743,6 +832,13 @@ ALTER TABLE `auth_users_permissions`
   ADD CONSTRAINT `auth_users_permissions_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
+-- Constraints for table `surat_izin`
+--
+ALTER TABLE `surat_izin`
+  ADD CONSTRAINT `FK_nik` FOREIGN KEY (`nik_pasien`) REFERENCES `pasien` (`nik_pasien`),
+  ADD CONSTRAINT `FK_nipKp` FOREIGN KEY (`nip_kapus`) REFERENCES `kapus` (`nip_kapus`);
+
+--
 -- Constraints for table `surat_kesehatan`
 --
 ALTER TABLE `surat_kesehatan`
@@ -756,6 +852,14 @@ ALTER TABLE `surat_rsa`
   ADD CONSTRAINT `FK_rsa_kapus` FOREIGN KEY (`nip_kapus`) REFERENCES `kapus` (`nip_kapus`),
   ADD CONSTRAINT `FK_rsa_pasien` FOREIGN KEY (`nik_pasien`) REFERENCES `pasien` (`nik_pasien`),
   ADD CONSTRAINT `FK_rsa_surat` FOREIGN KEY (`nomor_surat`) REFERENCES `surat_kesehatan` (`nomor_surat`);
+
+--
+-- Constraints for table `surat_rsaizin`
+--
+ALTER TABLE `surat_rsaizin`
+  ADD CONSTRAINT `FK_nik_pas` FOREIGN KEY (`nik_pasien`) REFERENCES `pasien` (`nik_pasien`),
+  ADD CONSTRAINT `FK_nip_kap` FOREIGN KEY (`nip_kapus`) REFERENCES `kapus` (`nip_kapus`),
+  ADD CONSTRAINT `FK_nomor_surat` FOREIGN KEY (`nomor_surat`) REFERENCES `surat_izin` (`nomor_surat`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
