e275f0e1438b021d8821ecfc3fedbc3ccccab4a4
muhamadzainul <zainulmustofa943@gmail.com>
2022-02-23 15:19:07 +0700

    data table surat izin

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -28,6 +28,10 @@
     <!-- Custom CSS -->
     <link href="<?= base_url(); ?>/css/style.min.css" rel="stylesheet" type="text/css">
 
+    <!-- datatable -->
+
+    <link href="/plugins/bower_components/datatables/jquery.dataTables.min.css" rel="stylesheet" type="text/css" />
+    <link href="https://cdn.datatables.net/buttons/1.2.2/css/buttons.dataTables.min.css" rel="stylesheet" type="text/css" />
     <!-- color CSS -->
     <link href="<?= base_url(); ?>/css/colors/megna.css" id="theme" rel="stylesheet" type="text/css">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -134,13 +138,20 @@
 
     <script src="/plugins/bower_components/datatables/jquery.dataTables.min.js"></script>
     <!-- start - This is for export functionality only -->
-    <script src="https://cdn.datatables.net/buttons/1.2.2/js/dataTables.buttons.min.js"></script>
+    <!-- <script src="https://cdn.datatables.net/buttons/1.2.2/js/dataTables.buttons.min.js"></script>
     <script src="https://cdn.datatables.net/buttons/1.2.2/js/buttons.flash.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
     <script src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/pdfmake.min.js"></script>
     <script src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/vfs_fonts.js"></script>
     <script src="https://cdn.datatables.net/buttons/1.2.2/js/buttons.html5.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.2.2/js/buttons.print.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.2.2/js/buttons.print.min.js"></script> -->
+    <script src="/plugins/DataTables/dataTables.buttons.min.js"></script>
+    <script src="/plugins/DataTables/buttons.flash.min.js"></script>
+    <script src="/plugins/DataTables/jszip.min.js"></script>
+    <script src="/plugins/DataTables/pdfmake.min.js"></script>
+    <script src="/plugins/DataTables/vfs_fonts.js"></script>
+    <script src="/plugins/DataTables/buttons.html5.min.js"></script>
+    <script src="/plugins/DataTables/buttons.print.min.js"></script>
     <!-- end - This is for export functionality only -->
     <script>
         $(document).ready(function() {
